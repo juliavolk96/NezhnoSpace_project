8a76817b658f97e32dbf7bc6331e56410d938daa
Julia <juliavolk96@gmail.com>
2023-08-17 12:42:02 +0400

    init: initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import './components/commonStyle.css';
+import './components/style.css';
+
+function App() {
+  const menuItems = [
+    { text: 'Главная', link: '#' },
+    { text: 'О нас', link: '#' },
+    { text: 'Блог', link: '#' },
+  ];
+  return (
+    <div className="App">
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Ubuntu+Condensed&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <Header menuItems={menuItems} />
+      <Footer menuItems={menuItems} />
+    </div>
+  );
+}
+
+export default App;
