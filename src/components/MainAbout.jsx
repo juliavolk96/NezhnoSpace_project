81297723d96d3551e03e669b9ac64763c66cfa15
Julia <juliavolk96@gmail.com>
2023-09-12 19:39:22 +0400

    fix: css & MainAbout

--- src/components/MainAbout.jsx
+++ src/components/MainAbout.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import aboutImage from '../images/photo.png';
+import aboutImageMobile from '../images/photo-mobile.png';
 
 function MainAbout() {
   return (
     <section className='main-about'>
-      <picture></picture>
+      <picture className='about-image'>
+        <source srcSet={aboutImage} type="image/png" />
+        <img src={aboutImageMobile} alt="girl" />
+      </picture>
       <div className='about-wrap'>
-        <h2 className='about-wrap-title'>Мы здесь, чтобы ты прошла свой путь нежно</h2>
+        <h2 className='about-wrap-title'>Мы здесь, чтобы<br />
+        ты прошла свой путь нежно</h2>
         <div className='about-wrap-description'>
           <p>Расстройство пищевого поведения влияет на многие аспекты жизни,<br />
-           и справиться с трудностями самостоятельно - не всегда получается.</p>
+            и справиться с трудностями самостоятельно - не всегда получается.</p>
            <p>Помни, поддержка - важное звено на пути к переменам.</p>
            <p>Наша платформа создана специально, чтобы быть рядом с тобой в любое время.</p>
         </div>
