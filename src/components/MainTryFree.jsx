858a698d39c5ca70191fe1a574ece0d5140ed678
Julia <juliavolk96@gmail.com>
2023-09-18 14:15:14 +0400

    fix: MainTryFree

--- src/components/MainTryFree.jsx
+++ src/components/MainTryFree.jsx
@@ -6,6 +6,13 @@ function MainTryFree() {
   return (
     <section className='try-free-wrap'>
       <h2 className='try-free-title'>Попробовать бесплатно:</h2>
+      <div className='try-free-description'>
+        <p>Уверены, что психотерапия должна быть доступна для каждого.<br />
+          После регистрации в личном кабинете тебе откроется бесплатный период,
+          который даст тебе возможность ознакомиться с нашими методиками и <span className='text-color-violet'>понять,
+          что подходит именно тебе</span>.
+        </p>
+      </div>
        <MainTryFreeSlider />
     </section>
   );
